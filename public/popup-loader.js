b9ac4b2a41dd6cf80a5be2db37d0d50930c70050
hyein89 <bihontai@gmail.com>
2025-10-28 13:53:07 +0700

    Create popup-loader.js

---
+++ public/popup-loader.js
@@ -0,0 +1,8 @@
+// popup-loader.js
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = "//bestsmartvpn.com/e4/d5/b1/e4d5b1ffbd6701f7bbc8c3bfc7659bd4.js";
+  document.body.appendChild(s);
+})();
