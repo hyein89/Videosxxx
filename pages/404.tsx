faf482a2c4acd6fed98a09f892db22edc91ca957
hyein89 <bihontai@gmail.com>
2025-10-28 15:01:04 +0700

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -14,8 +14,8 @@ export default function Custom404() {
       padding: "20px"
     }}>
       <h1 style={{ fontSize: "4rem", marginBottom: "20px" }}>404</h1>
-      <h2 style={{ marginBottom: "20px" }}>Video tidak ditemukan!</h2>
-      <p style={{ marginBottom: "20px" }}>Maaf, video yang kamu cari tidak tersedia.</p>
+      <h2 style={{ marginBottom: "20px" }}>Video not found!</h2>
+      <p style={{ marginBottom: "20px" }}>Sorry, the video you are looking for is not available..</p>
       <Link href="/">
         <button style={{
           padding: "10px 20px",
@@ -26,7 +26,7 @@ export default function Custom404() {
           borderRadius: "5px",
           cursor: "pointer"
         }}>
-          Kembali ke Beranda
+          Back home
         </button>
       </Link>
     </div>
