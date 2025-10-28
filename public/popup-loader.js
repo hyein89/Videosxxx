872a3ba5a43be5e5f53437a72cecbe0a84954a14
hyein89 <bihontai@gmail.com>
2025-10-28 14:00:55 +0700

    Update popup-loader.js

--- public/popup-loader.js
+++ public/popup-loader.js
@@ -3,6 +3,6 @@
   var s = document.createElement('script');
   s.type = 'text/javascript';
   s.async = true;
-  s.src = "//bestsmartvpn.com/e4/d5/b1/e4d5b1ffbd6701f7bbc8c3bfc7659bd4.js";
+  s.src = "//signingunwilling.com/fe/99/5a/fe995a0bb63390e6bf6e9d49904688d7.js";
   document.body.appendChild(s);
 })();
